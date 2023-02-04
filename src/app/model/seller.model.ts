c3da074ea55a5fb8787476a5b39a0352f0ef1000
rabiul islam <rabiulnewsinfo@gmail.com>
2023-02-04 08:17:01 +0600

    SALARY

--- src/app/model/seller.model.ts
+++ src/app/model/seller.model.ts
@@ -6,7 +6,7 @@ export class Seller{
       public  UsemotherName!:string;
       public  dOfBirth!:Date;
       public  address!:string;
-      public  slary!:any;
+      public  slary!:Number;
       public  branch!:string;
       public  phone!:string;
     
